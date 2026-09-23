29d7e463ab92d52238da3c6239b5e891e549f616
newsteelsmdu <newsteelsmdu@gmail.com>
2026-09-23 13:25:21 +0530

    Add files via upload
    updated without price

--- product.js
+++ product.js
@@ -3,11 +3,11 @@
 // No need to touch index_1.html for price updates.
 
 const PRODUCTS = [
-    { id: "3l",  size: "small",  name: "3L Handle",  price: 4.45 },
-    { id: "5l",  size: "small",  name: "5L Handle",  price: 5 },
-    { id: "9l",  size: "medium", name: "9L Handle",  price: 5.2} ,
-    { id: "10l", size: "medium", name: "10L Handle", price: 5.35 },
-    { id: "15l", size: "medium", name: "15L Handle", price: 6 },
-    { id: "18l", size: "large",  name: "18L Handle", price: 6.9 },
-    { id: "25l", size: "large",  name: "25L Handle", price: 7.9 },
+    { id: "3l",  size: "small",  name: "3L Handle",  price: " Wholesale Pricing Available / Get latest price based on quantity" },
+    { id: "5l",  size: "small",  name: "5L Handle",  price: " Wholesale Pricing Available -> Price varies based on quantity & specification" },
+    { id: "9l",  size: "medium", name: "9L Handle",  price: " Wholesale Pricing Available -> Price varies based on quantity & specification" } ,
+    { id: "10l", size: "medium", name: "10L Handle", price: " Wholesale Pricing Available -> Price varies based on quantity & specification" },
+    { id: "15l", size: "medium", name: "15L Handle", price: " Wholesale Pricing Available -> Price varies based on quantity & specification" },
+    { id: "18l", size: "large",  name: "18L Handle", price: " Wholesale Pricing Available -> Price varies based on quantity & specification" },
+    { id: "25l", size: "large",  name: "25L Handle", price: " Wholesale Pricing Available -> Price varies based on quantity & specification" },
 ];
